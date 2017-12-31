604d35e48bd95104a2dc197ccfd41fd89a90b5c1
Sam Kessaram <samkessaram@gmail.com>
2017-12-31 18:53:38 -0400

    install and configure webpack

--- app/index.js
+++ app/index.js
@@ -3,5 +3,17 @@ var ReactDOM = require('react-dom');
 require('./index.css');
 
 class App extends React.Component {
-
+  render() {
+    return (
+      <div>
+        Hello World!
+      </div>
+    )
+  }
 }
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
